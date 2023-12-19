67d9c0633291c7d8903f2cb17aa07e7a07dbca35
Shahar Best <bestshahar9@gmail.com>
2023-12-19 21:45:46 +0200

    done routes

--- routing/src/App.jsx
+++ routing/src/App.jsx
@@ -10,13 +10,17 @@ const books = [
 export default function App() {
   return <>
     <NavBar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/books" element={<Catalog />} />
-      <Route path="/books/:id" element={<BookPage />} />
-    </Routes>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/books" element={<Catalog />} />
+        <Route path="/books/:id" element={<BookPage />} />
+        <Route path="/books/new" element={<NewBook />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
   </>;
 }
 
@@ -27,8 +31,7 @@ function NavBar() {
       <li><Link to="/about">About</Link></li>
       <li><Link to="/contact">Contact</Link></li>
       <li><Link to="/books">Catalog</Link></li>
-      <li><Link to="/books/101">first book</Link></li>
-      <li><Link to="/books/102">second book</Link></li>
+      <li><Link to="/books/new">New Book</Link></li>
     </ul>
   </nav>;
 }
@@ -38,12 +41,26 @@ function BookPage() {
 
   const book = books.find(b => b.id === id);
 
-	return <>
+  return <>
     <pre>{JSON.stringify(book, null, 2)}</pre>
   </>
 }
 
-function Catalog() { return <h1>Catalog</h1>; }
+function Catalog() {
+  const bookItems = books.map(b => <li>
+    <Link to={`/books/${b.id}`}>{b.name}</Link>
+  </li>);
+
+  return <>
+    <h1>Catalog</h1>
+    <ul className="catalog-list">
+      {bookItems}
+    </ul>
+  </>;
+}
+
+function NotFound() { return <h1>404</h1>; }
+function NewBook() { return <h1>New Book</h1>; }
 function Home() { return <h1>Home</h1>; }
 function About() { return <h1>About</h1>; }
 function Contact() { return <h1>Contact</h1>; }
