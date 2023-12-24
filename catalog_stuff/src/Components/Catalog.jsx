d000db265dbecbcc88b2e0cbf147fcdd7b7296e5
Shahar Best <bestshahar9@gmail.com>
2023-12-24 20:54:38 +0200

    sort feature

--- catalog_stuff/src/Components/Catalog.jsx
+++ catalog_stuff/src/Components/Catalog.jsx
@@ -5,17 +5,32 @@ import CatalogBoard from './CatalogBoard';
 const genreOptions =
   ['', ...Array.from(new Set(allBooks.map(b => b.genre)))];
 
+const sortOptions = [ '', 'name', 'price', 'genre' ];
+
 export default function Catalog() {
   const [books, setBooks] = useState(allBooks);
   const [nameInputVal, setNameInputVal] = useState('');
-  const [genreInputVal, setGenreInputVal] = useState('');
+  const [genreChosen, setGenreChosen] = useState('');
+  const [sortChosen, setSortChosen] = useState('');
 
   function handleSubmit(e) {
     e.preventDefault();
 
     const onlySearchedBooks = allBooks.filter(b =>
       b.name.includes(nameInputVal)
-        && b.genre.includes(genreInputVal));
+        && b.genre.includes(genreChosen));
+    
+    switch (sortChosen) {
+      case 'price':
+        onlySearchedBooks.sort((b1, b2) => b1.price - b2.price);
+        break;
+      case 'name':
+        onlySearchedBooks.sort((b1, b2) => b1.name.localeCompare(b2.name));
+        break;
+      case 'genre':
+        onlySearchedBooks.sort((b1, b2) => b1.genre.localeCompare(b2.genre));
+        break;
+    }
 
     setBooks(onlySearchedBooks);
   }
@@ -32,9 +47,16 @@ export default function Catalog() {
 
       <label htmlFor="">genre</label>
       <select
-        value={genreInputVal}
-        onChange={e => setGenreInputVal(e.target.value)}>
+        value={genreChosen}
+        onChange={e => setGenreChosen(e.target.value)}>
         {genreOptions.map(g => <option key={g} value={g}>{g}</option>)}
+      </select>
+
+      <label htmlFor="">sort category</label>
+      <select
+        value={sortChosen}
+        onChange={e => setSortChosen(e.target.value)}>
+        {sortOptions.map(s => <option key={s} value={s}>{s}</option>)}
       </select>
 
       <button>submit</button>
--- catalog_stuff/src/Components/CatalogBoard.jsx
+++ catalog_stuff/src/Components/CatalogBoard.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 
-const aviPrefix = "https://fscourse.shaharbest.com/img/avi";
+// const aviPrefix = "https://fscourse.shaharbest.com/img/avi";
+const imageUrlPrefix = "https://picsum.photos/id";
+// 
 
 export default function CatalogBoard({ books }) {
     const cards = books.map(b =>
@@ -11,7 +13,7 @@ export default function CatalogBoard({ books }) {
     </div>;
 }
 
-function BookCard({ book: { id, name, price, imgID, genre } }) {
+function BookCard({ book: { id, name, price, genre } }) {
     return <div className="book-card">
         <h3>
           <Link to={`/books/${id}`}>
@@ -19,12 +21,12 @@ function BookCard({ book: { id, name, price, imgID, genre } }) {
           </Link>
         </h3>
         <img className="book-image"
-          src={getBookImageUrl(imgID)} alt={`image of ${name}`} />
+          src={getBookImageUrl(id)} alt={`image of ${name}`} />
         <div className="book-genere">{genre}</div>
         <div className="book-price">$ {price}</div>
     </div>
 }
 
 function getBookImageUrl(imageID) {
-  return `${aviPrefix}/${imageID}.webp`
+  return `${imageUrlPrefix}/${imageID}/200/200`
 }
