298a8e17e3b8696e11d11bb9c09c9b8026138b86
Shahar Best <bestshahar9@gmail.com>
2023-12-26 19:41:55 +0200

    home demo

--- effects/src/Components/Home.jsx
+++ effects/src/Components/Home.jsx
@@ -1,5 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
-    return <>
-        <h1>Home</h1>
-    </>
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        getStamData().then(result => setData(result));
+    }, []);
+
+    return <Comp data={data} />;
+}
+
+function Comp({ data }) {
+    return !data ?
+        'Loading...' :
+        <pre>{JSON.stringify(data, null, 2)}</pre>;
+}
+
+async function getStamData() {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users/1';
+    const res = await fetch(apiUrl);
+    return await res.json();
 }
