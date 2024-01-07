1b93e3225029dddd64bdbd0a21fc5dfb15b16d8c
Shahar Best <bestshahar9@gmail.com>
2024-01-07 18:29:21 +0200

    context demo template

--- context_demo/src/App.jsx
+++ context_demo/src/App.jsx
@@ -1,3 +1,24 @@
 export default function App() {
-  return 'hi';
+  return <>
+    <header>
+      <h1>My Website</h1>
+    </header>
+    <main>
+      <article>
+        <h2>title 1</h2>
+        <p>blah blah blah</p>
+      </article>
+      <article>
+        <h2>title 1</h2>
+        <p>blah blah blah</p>
+      </article>
+      <article>
+        <h2>title 1</h2>
+        <p>blah blah blah</p>
+      </article>
+    </main>
+    <footer>
+      <p>all rights reseved to me</p>
+    </footer>
+  </>;
 }
