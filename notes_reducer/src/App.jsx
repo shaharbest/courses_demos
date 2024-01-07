bdb1e3f82bbf5a1dc15ba5439a8e337ead044060
Shahar Best <bestshahar9@gmail.com>
2024-01-07 21:23:55 +0200

    delete selected

--- notes_reducer/src/App.jsx
+++ notes_reducer/src/App.jsx
@@ -7,7 +7,8 @@ const defaultNotes = [
 ];
 
 export default function App() {
-  const [notes, setNotes] = useState(defaultNotes);
+  const [notes, setNotes] =
+    useState(defaultNotes.map(n => ({ ...n, isSelected: false })));
   const [newNoteInputVal, setNewNoteInputVal] = useState('');
 
   function handleDeleteNote(noteIndexToDelete) {
@@ -15,33 +16,57 @@ export default function App() {
       index !== noteIndexToDelete));
   }
 
-  function handleNewNote(event) {
-    event.preventDefault();
+  function handleNoteSelected(index, isSelected) {
+    const notesClone = structuredClone(notes);
+    notesClone[index].isSelected = isSelected;
+    setNotes(notesClone)
+  }
+
+  function handleDeleteSelected() {
+    setNotes(notes.filter(n => !n.isSelected));
+  }
+
+  function handleNewNote() {
     setNotes([ ...notes, { content: newNoteInputVal } ]);
     setNewNoteInputVal('');
   }
 
   const notesElements = notes.map((n, index) =>
     <Note key={index} noteData={n}
-      onDeleteNote={() => handleDeleteNote(index)} />);
+      onDeleteNote={() => handleDeleteNote(index)}
+      onChangeSelected={isSelected => handleNoteSelected(index, isSelected)} />);
 
   return <>
     <h1>Notes</h1>
-    <form onSubmit={handleNewNote}>
-      <input type="text"
-        value={newNoteInputVal}
-        onChange={e => setNewNoteInputVal(e.target.value)} />
-      <button>add note</button>
-    </form>
+    <div className="notes-board-controls">
+      <form>
+        <input type="text"
+          value={newNoteInputVal}
+          onChange={e => setNewNoteInputVal(e.target.value)} />
+        <button onClick={handleNewNote} type="button">
+          add note
+        </button>
+      </form>
+      <form>
+        <button onClick={handleDeleteSelected} type="button">
+          delete selected
+        </button>
+      </form>
+    </div>
     <div className="notes-board">
       {notesElements}
     </div>
   </>;
 }
 
-function Note({ noteData, onDeleteNote }) {
+function Note({ noteData, onDeleteNote, onChangeSelected }) {
   return <div className="note">
-    <button onClick={onDeleteNote}>x</button>
+    <div className="note-header">
+      <button onClick={onDeleteNote}>x</button>
+      <input type="checkbox"
+        onChange={e => onChangeSelected(e.target.checked)}
+        checked={noteData.isSelected} />
+    </div>
     <p>{noteData.content}</p>
   </div>;
 }
